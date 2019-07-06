276baf1211061940e803feefde17dd9dff7bb8ea
nyamada00 <ex-yamada@hotmail.co.jp>
2019-07-06 03:02:02 +0000

    State

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -34,15 +34,40 @@ object RNG {
     (if (i < 0) -(i + 1) else i, r)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
 
-  def intDouble(rng: RNG): ((Int, Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
 
-  def doubleInt(rng: RNG): ((Double, Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i,d),r)=intDouble(rng)
+    ((d,i),r)
+  }
 
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1,r1)=double(rng)
+    val (d2,r2)=double(r1)
+    val (d3,r3)=double(r2)
+    ((d1,d2,d3),r3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(cnt:Int,xs:List[Int],r:RNG):(List[Int],RNG)={
+      if(cnt==0) 
+        (xs,r)
+      else{
+        val (x,r1)=r.nextInt
+        go(cnt-1,x::xs,r1)
+      }
+    }
+    go(count,List(),rng)
+  }
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
